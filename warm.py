2b504baf035c3b1154d7c374587d95f85bcc8a3f
nerddevviscool <sigmarizler1012@gmail.com>
2024-08-01 13:21:18 -0700

    Create warm.py

---
+++ warm.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a + b
+print('add:' + str(add(4, 1)))
+
+
+def add2(t, o):
+    return t * o
+print('add2:' + str(add2(3, 1)))
+
+def add3(b, e):
+    return b / e
+print("add3:" + str(add3(7, 2)))
